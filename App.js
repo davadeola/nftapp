b62f64d49e3a01b3cfbcadced76dd754e0a27171
davadeola <adeoladavid15@gmail.com>
2022-05-30 21:43:12 +0300

    added custom fonts

--- App.js
+++ App.js
@@ -17,8 +17,18 @@ const theme = {
 const Stack = createStackNavigator();
 
 export default function App() {
+  const [loaded] = useFonts({
+    InterBold: require("./assets/fonts/Inter-Bold.ttf"),
+    InterSemiBold: require("./assets/fonts/Inter-SemiBold.ttf"),
+    InterMedium: require("./assets/fonts/Inter-Medium.ttf"),
+    InterRegular: require("./assets/fonts/Inter-Regular.ttf"),
+    InterLight: require("./assets/fonts/Inter-Light.ttf"),
+  });
+
+  if (!loaded) return null;
+
   return (
-    <NavigationContainer>
+    <NavigationContainer theme={theme}>
       <Stack.Navigator
         screenOptions={{
           headerShown: false,
